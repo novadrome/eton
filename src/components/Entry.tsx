5b21f44ef5ce0f1967b84c9c16d20deb2eab8077
novadrome <25754143+novadrome@users.noreply.github.com>
2021-04-15 16:39:38 -0400

    initial commit

---
+++ src/components/Entry.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const Entry = (props:any) => {
+    return(
+        <TextBox id={props.id} contentEditable="true"/>
+    ) 
+}
+
+const TextBox = styled.div`
+
+    border: solid 1px var(--secondary-color);
+    background-color: var(--entry-background-color);
+    color: var(--secondary-color);
+    outline: none;
+
+    font-size: 14pt;
+    font-family: "Century Gothic";
+    overflow-wrap: break-word;
+    overflow: auto;
+    white-space: normal;	
+            
+    width: 50%;
+    max-width: 50%;
+
+    min-height: 1em;
+    height: auto;
+    overflow-y: auto;
+
+    resize: none;
+    padding: 1em;
+
+    transition: all 0.1s ease-in-out;
+`;
+
+export default Entry;
